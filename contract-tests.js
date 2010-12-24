d60fbb941fd6f0bc3a7087544b35388b21dbb93a
mista_k <mista_k@z-inc.net>
2010-12-24 14:27:35 +0500

    выделил встроенные валидаторы в отдельный класс

--- contract-tests.js
+++ contract-tests.js
@@ -19,14 +19,46 @@ $(function () {
                 max: 15
             }
         ],
+        validators: {
+            dummy: function (constraint, value) {
+                return true;
+            },
+            fail: function (constraint, value) {
+                return false;
+            }
+        },
         log: function(msg) {
             ok(false, msg);
         }
     };
 
-    var fc = Contract(func, opts);
 
     module("Contract");
+
+    test("Validators", function () {
+        // пустой конструктор
+        var v1 = new Contract.Validators();
+        deepEqual(v1, Contract.Validators.prototype, "оригинальные валидаторы");
+        // конструктор с дополнительными валидаторами
+        var v2 = new Contract.Validators(opts.validators);
+        notDeepEqual(v2, Contract.Validators.prototype, "дополнительные валидаторы");
+        equal(v2.dummy, opts.validators.dummy, "валидатор dummy");
+        equal(v2.fail, opts.validators.fail, "валидатор fail");
+        // массив в качестве параметра
+        var v3 = new Contract.Validators([
+            function () {},
+            function () {},
+            function () {}
+        ]);
+        ok(v3[0], "валидатор с именем 0 существует");
+        ok(v3[1], "валидатор с именем 1 существует");
+        ok(v3[2], "валидатор с именем 2 существует");
+        ok(!v3[3], "валидатор с именем 3 не существует");
+    });
+
+
+
+    var fc = Contract(func, opts);
 
     test("base", function () {
         fc(1,2);
--- contract.js
+++ contract.js
@@ -8,18 +8,17 @@
 
     var PHASE_IN = "in",
         PHASE_OUT = "out",
-        ARGUMENTS_LENGTH = "argumentsLength";
+        ARGUMENTS_LENGTH = "argumentsLength",
+        C = {}; // local namespace
 
     function extend(obj, opts) {
         var name, copy;
-
         for (name in opts) if (opts.hasOwnProperty(name)) {
             copy = opts[name];
             if (copy !== undefined) {
                 obj[name] = copy;
             }
         }
-
         return obj;
     }
 
@@ -32,107 +31,103 @@
         return s.join(".");
     }
 
-    // конструктор для экземпляра контракта
-    var ContractProxy = function (fn, opts) {
-        var o;
-        if (opts && opts.validators) {
-            // дополним существующие валидаторы не удаляя остальные
-            o = extend({}, opts);
-            o.validators = extend(extend({}, this.validators), o.validators);
-        } else {
-            o = opts;
-        }
-        extend(this, o || {});
 
+    var V = {
+        "blank":        // пустая / непустая строка (Boolean)
+                function (constraint, value) {
+                    value = ("" + value).length > 0; // convert to string and chech length
+                    return constraint || value;
+                },
+        "inList":       // вхождение в список (Array of strings)
+                function (constraint, value) {
+                    var i, ret = false;
+                    for (i = 0; i < constraint.length; i++) {
+                        if (constraint[i] == value) {
+                            ret = true;
+                            break;
+                        }
+                    }
+                    return ret;
+                },
+        "matches":      // соответствие регекспу (Regexp string)
+                function (constraint, value) {
+                    // все слеши должны быть экранированы
+                    var rx = new RegExp(constraint);
+                    return rx.test(value);
+                },
+        "max":          // проверка на максимум (Float)
+                function (constraint, value) {
+                    return value <= constraint;
+                },
+        "min":          // проверка на минимум (Float)
+                function (constraint, value) {
+                    return value >= constraint;
+                },
+        "notEqual":     // не равно (любой тип, скорее всего String)
+                function (constraint, value) {
+                    return value != constraint;
+                },
+        "nullable":     // null / не null (Boolean)
+                function (constraint, value) {
+                    return constraint || !!value;   // convert any not null value to true
+                },
+        "range":        // диапазон (Array of floats)
+                function (constraint, value) {
+                    return (value >= constraint[0] && value <= constraint[1]);
+                },
+        "scale":        // количество знаков после запятой (Integer)
+                function (constraint, value) {
+                    var pattern = /^-?\d+(\.\d*)?$/,
+                            fv = parseFloat(value); // convert to float
+                    return !isNaN(fv) &&
+                            pattern.test("" + value) &&
+                            parseFloat(fv.toFixed(constraint)) === fv;
+                },
+        "size":         // размер строки (Integer или Array of integers)
+                function (constraint, value) {
+                    var len = ("" + value).length;
+                    return (Object.prototype.toString.call(constraint) === "[object Array]") ?
+                            V.range(constraint, len) : (constraint == len);
+                },
+        "maxSize":      // максимальный размер строки (Integer)
+                function (constraint, value) {
+                    var len = ("" + value).length;
+                    return len <= constraint;
+                },
+        "minSize":      // минимальный размер строки (Integer)
+                function (constraint, value) {
+                    var len = ("" + value).length;
+                    return len >= constraint;
+                }
+    };
+
+    C.Validators = function (opts) {
+        extend(this, typeof opts === "object" ? opts : {});
+    };
+
+    extend(C.Validators.prototype, V);
+
+
+    // конструктор для экземпляра контракта
+    C.Proxy = function (fn, opts) {
+        this.args = opts.args;
+        this.result = opts.result;
+        this.log = opts.log;
+        this.validators = new C.Validators(opts.validators);
         this.fn = fn;
     };
 
-    extend(ContractProxy.prototype, {
+    extend(C.Proxy.prototype, {
         // список контрактов для аргументов функции
         args: [],
 
         // список контрактов для результата
         result: [],
 
-        // набор готовых именованных валидаторов
-        validators: {
-            "blank":        // пустая / непустая строка (Boolean)
-                    function (constraint, value) {
-                        value = ("" + value).length > 0; // convert to string and chech length
-                        return constraint || value;
-                    },
-            "inList":       // вхождение в список (Array of strings)
-                    function (constraint, value) {
-                        var i, ret = false;
-                        for (i = 0; i < constraint.length; i++) {
-                            if (constraint[i] == value) {
-                                ret = true;
-                                break;
-                            }
-                        }
-                        return ret;
-                    },
-            "matches":      // соответствие регекспу (Regexp string)
-                    function (constraint, value) {
-                        // все слеши должны быть экранированы
-                        var rx = new RegExp(constraint);
-                        return rx.test(value);
-                    },
-            "max":          // проверка на максимум (Float)
-                    function (constraint, value) {
-                        return value <= constraint;
-                    },
-            "min":          // проверка на минимум (Float)
-                    function (constraint, value) {
-                        return value >= constraint;
-                    },
-            "notEqual":     // не равно (любой тип, скорее всего String)
-                    function (constraint, value) {
-                        return value != constraint;
-                    },
-            "nullable":     // null / не null (Boolean)
-                    function (constraint, value) {
-                        return constraint || !!value;   // convert any not null value to true
-                    },
-            "range":        // диапазон (Array of floats)
-                    function (constraint, value) {
-                        return (value >= constraint[0] && value <= constraint[1]);
-                    },
-            "scale":        // количество знаков после запятой (Integer)
-                    function (constraint, value) {
-                        var pattern = /^-?\d+(\.\d*)?$/,
-                                fv = parseFloat(value); // convert to float
-                        return !isNaN(fv) &&
-                                pattern.test("" + value) &&
-                                parseFloat(fv.toFixed(constraint)) === fv;
-                    },
-            "size":         // размер строки (Integer или Array of integers)
-                    function (constraint, value) {
-                        var len = ("" + value).length;
-                        return (Object.prototype.toString.call(constraint) === "[object Array]") ?
-                                Constraints.range(constraint, len) : (constraint == len);
-                    },
-            "maxSize":      // максимальный размер строки (Integer)
-                    function (constraint, value) {
-                        var len = ("" + value).length;
-                        return len <= constraint;
-                    },
-            "minSize":      // минимальный размер строки (Integer)
-                    function (constraint, value) {
-                        var len = ("" + value).length;
-                        return len >= constraint;
-                    },
-            "validator":    // кастомный js валидатор (Function или String)
-                    function (constraint, value) {
-                        var ret = false, func;
-                        try {
-                            func = (constraint instanceof Function) ? constraint : new Function("value", constraint);
-                            ret = func(value);
-                        } catch (ex) {}
-                        return ret;
-                    }
-        },
+        // набор именованных валидаторов
+        validators: {},
 
+        // логгирование сверок с контрактом
         log: function (msg) {
             if (console && console.log) {
                 console.log(msg);
@@ -156,7 +151,7 @@
             for (i = 0; i < data.length; i++) {
                 if (opts[i]) {
                     contracts = opts[i];
-                    for (name in contracts) if (contracts.hasOwnProperty(name) && this.validators.hasOwnProperty(name)) {
+                    for (name in contracts) if (contracts.hasOwnProperty(name) && this.validators[name]) {
                         if (!this.validators[name](contracts[name], data[i])) {
                             this.log(stringBuilder(phase, name, i));
                         }
@@ -175,7 +170,7 @@
      */
     window.Contract = function (fn, opts) {
 
-        var contract = new ContractProxy(fn, opts);
+        var contract = new C.Proxy(fn, opts);
 
         return function () {
             var result;
@@ -186,6 +181,6 @@
         };
     };
 
-    window.ContractProxy = ContractProxy;
+    extend(window.Contract, C);
 
 }(window));
